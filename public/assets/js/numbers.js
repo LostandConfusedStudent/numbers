3c75288b3a66b7e4328ade7dbb42be138db46dd8
pausun <pfsun9999@msn.com>
2020-10-19 18:50:31 -0800

    Created js file and explored console.log and first function

---
+++ public/assets/js/numbers.js
@@ -0,0 +1,21 @@
+console.log("hello");
+console.log("1 + 2 = " + (1 + 2));
+console.log("-----------------------");
+
+var x = 1;
+var y = 8;
+console.log("When x = 1 and y = 8, and then you add them together:")
+console.log(x + y);
+console.log("-----------------------");
+
+var x = 5;
+var y = 8;
+var z = 12;
+
+function addition(x, y, z) {
+    return x + y + z;
+}
+
+var result = addition(x, y, z);
+console.log(" 5 + 8 + 12 = " + result);
+console.log("-----------------------");
