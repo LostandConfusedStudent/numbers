920d0ea137f0435a096e90e99b0be17fbf595eb7
pausun <pfsun9999@msn.com>
2020-10-23 08:40:34 -0800

    Added function for player and house random hands

--- public/assets/js/game.js
+++ public/assets/js/game.js
@@ -1,8 +1,16 @@
 // Declare cards array
 var cards = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"];
 
-for (var i = 0; i < cards.length; i++) {
-    var draw = Math.floor(Math.random() * cards.length);
-    console.log("You just got a " + (draw, cards[draw]));
+function draw() {
+    for (var i = 0; i < 2; i++) {
+        var playerCards = Math.floor(Math.random() * cards.length);
+        console.log("You just got a " + (playerCards, cards[playerCards]));
+    };
 
-}
+    for (var i = 0; i < 2; i++) {
+        var houseCards = Math.floor(Math.random() * cards.length);
+        console.log("Dealer just got a " + (houseCards, cards[houseCards]));
+    };
+};
+
+draw();
